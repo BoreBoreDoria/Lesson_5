0a7dbf78f5e538631336e94aceccf0ed35901697
Nikita Rochev <nikita.rochev@teamup-soft.ru>
2023-01-12 19:42:28 +0300

    Модификаторы доступа

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import ru.sbrf.chat.util.Animal;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+
+        animal.setText4("Text");
+        System.out.println(animal.getText4());
+
+    }
+}
---
+++ src/ru/sbrf/chat/util/Cat.java
@@ -0,0 +1,13 @@
+package ru.sbrf.chat.util;
+
+
+public class Cat extends Animal {
+
+    void test() {
+        publ();
+        prot();
+        def();
+
+    }
+
+}
